bbd1834d346707f2b54d9be3ccfb2250e3ed3b49
kazuikios <slash0juice@gmail.com>
2022-01-31 19:02:55 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	log.Println("broad cast start")
+	bot, err := linebot.New("LineBotSecret", "LineChannelAccessToken")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	message1 := linebot.NewTextMessage("mazi test.")
+	message2 := linebot.NewTextMessage("https://www.youtube.com/watch?v=M4sWFgBYNbI")
+	if _, err := bot.BroadcastMessage(message1, message2).Do(); err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("broad cast end")
+}
