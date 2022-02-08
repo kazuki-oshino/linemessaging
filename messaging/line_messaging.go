cd89454894902c426e5a77ffb9c35f3414a11f7e
kazuikios <slash0juice@gmail.com>
2022-02-08 22:41:24 +0900

    add more movie variation.

--- messaging/line_messaging.go
+++ messaging/line_messaging.go
@@ -2,6 +2,7 @@ package messaging
 
 import (
 	"log"
+	"math/rand"
 	"os"
 	"time"
 
@@ -16,6 +17,15 @@ const (
 
 	// SeroriURL is セロリのURL
 	SeroriURL = "https://www.youtube.com/watch?v=M4sWFgBYNbI"
+
+	// KirariURL is きらりのURL
+	KirariURL = "https://www.youtube.com/watch?v=TcLLpZBWsck"
+
+	// HatoURL is 鳩のURL
+	HatoURL = "https://www.youtube.com/watch?v=JQVKkjA0law"
+
+	// HigeSoriURL is ひげそりのURL
+	HigeSoriURL = "https://www.youtube.com/shorts/irk3VjWnzQA"
 )
 
 // TargetLatestMovie is movie info struct.
@@ -85,7 +95,18 @@ func Execute() {
 	if feed.publishedDate.Add(time.Hour*9).Day() == time.Now().UTC().Add(time.Hour*9).Day() {
 		broadCastInfo = NewBroadCastInfo("ホモサピの最新動画が来ているよ！", feed.url)
 	} else {
-		broadCastInfo = NewBroadCastInfo("今日はホモサピの動画あがっていないよ・・・代わりにパセリ聞いてね", SeroriURL)
+		rand.Seed(time.Now().UnixNano())
+		switch rand.Intn(3) {
+		case 0:
+			broadCastInfo = NewBroadCastInfo("今日はきらりの日やで", KirariURL)
+		case 1:
+			broadCastInfo = NewBroadCastInfo("鳩ってええよな", HatoURL)
+		case 2:
+			broadCastInfo = NewBroadCastInfo("今日はひげそりの日やで", HigeSoriURL)
+		default:
+			broadCastInfo = NewBroadCastInfo("今日はホモサピの動画あがっていないよ・・・代わりにパセリ聞いてね", SeroriURL)
+		}
+
 	}
 	broadCastInfo.BroadCast()
 }
