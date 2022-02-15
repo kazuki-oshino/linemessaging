11acd33e827d9f48acde009245c80955b828a0bb
kazuikios <slash0juice@gmail.com>
2022-02-15 09:58:46 +0900

    change daily movie

--- messaging/line_messaging.go
+++ messaging/line_messaging.go
@@ -2,7 +2,6 @@ package messaging
 
 import (
 	"log"
-	"math/rand"
 	"os"
 	"time"
 
@@ -12,20 +11,14 @@ import (
 )
 
 const (
-	// TargetURL is ホモサピ動画FEED
-	TargetURL = "https://www.youtube.com/feeds/videos.xml?channel_id=UCd0hscDvJvzRbo8Rk7JPQMA"
+	// HomosapiFeedURL is ホモサピ動画FEED URL
+	HomosapiFeedURL = "https://www.youtube.com/feeds/videos.xml?channel_id=UCd0hscDvJvzRbo8Rk7JPQMA"
 
 	// SeroriURL is セロリのURL
 	SeroriURL = "https://www.youtube.com/watch?v=M4sWFgBYNbI"
 
-	// KirariURL is きらりのURL
-	KirariURL = "https://www.youtube.com/watch?v=TcLLpZBWsck"
-
-	// HatoURL is 鳩のURL
-	HatoURL = "https://www.youtube.com/watch?v=JQVKkjA0law"
-
-	// HigeSoriURL is ひげそりのURL
-	HigeSoriURL = "https://www.youtube.com/shorts/irk3VjWnzQA"
+	// HigeSoriFeedURL is ひげそりのFEED URL
+	HigeSoriFeedURL = "https://www.youtube.com/feeds/videos.xml?channel_id=UCVI4ZUakZBLvdgb0ltKPS8Q"
 )
 
 // TargetLatestMovie is movie info struct.
@@ -48,7 +41,7 @@ func NewTarget(item gofeed.Item) *TargetLatestMovie {
 func GetTargetLatestMovieByFeed(feedURL string) *TargetLatestMovie {
 	feed, err := gofeed.NewParser().ParseURL(feedURL)
 	if err != nil {
-		log.Fatal("failed to parsefeed URL.")
+		log.Fatal("failed to parse feed URL.")
 	}
 
 	if len(feed.Items) == 0 {
@@ -87,26 +80,34 @@ func (b *BroadcastInfo) BroadCast() {
 	}
 }
 
+// checkTargetAndBroadCast is check target latest movie, if published date is today broadcast this movie.
+func checkTargetAndBroadCast(targetURL string, foundMessage string, timeDiff int) bool {
+	feed := GetTargetLatestMovieByFeed(targetURL)
+	var broadCastInfo *BroadcastInfo
+	if feed.publishedDate.Add(time.Hour*time.Duration(timeDiff)).Day() == time.Now().UTC().Add(time.Hour*9).Day() {
+		broadCastInfo = NewBroadCastInfo(foundMessage, feed.url)
+		broadCastInfo.BroadCast()
+		log.Println(foundMessage)
+		return true
+	}
+	return false
+}
+
 // Execute is to execute Line Messaging API to push message.
 func Execute() {
 	godotenv.Load(".env")
-	feed := GetTargetLatestMovieByFeed(TargetURL)
-	var broadCastInfo *BroadcastInfo
-	if feed.publishedDate.Add(time.Hour*9).Day() == time.Now().UTC().Add(time.Hour*9).Day() {
-		broadCastInfo = NewBroadCastInfo("ホモサピの最新動画が来ているよ！", feed.url)
-	} else {
-		rand.Seed(time.Now().UnixNano())
-		switch rand.Intn(3) {
-		case 0:
-			broadCastInfo = NewBroadCastInfo("今日はきらりの日やで", KirariURL)
-		case 1:
-			broadCastInfo = NewBroadCastInfo("鳩ってええよな", HatoURL)
-		case 2:
-			broadCastInfo = NewBroadCastInfo("今日はひげそりの日やで", HigeSoriURL)
-		default:
-			broadCastInfo = NewBroadCastInfo("今日はホモサピの動画あがっていないよ・・・代わりにパセリ聞いてね", SeroriURL)
-		}
 
+	isHomosapiBroadCast := checkTargetAndBroadCast(HomosapiFeedURL, "ホモサピの動画あったよ！嬉しいね！", 9)
+	if isHomosapiBroadCast {
+		return
 	}
+
+	isHigeBroadCast := checkTargetAndBroadCast(HigeSoriFeedURL, "ひげの最新動画来たでー", 0)
+	if isHigeBroadCast {
+		return
+	}
+
+	broadCastInfo := NewBroadCastInfo("今日はホモサピもひげもないよ・・・代わりにパセリ聞いてね", SeroriURL)
 	broadCastInfo.BroadCast()
+	log.Println("今日はホモサピもひげもないよ・・・代わりにパセリ聞いてね")
 }
